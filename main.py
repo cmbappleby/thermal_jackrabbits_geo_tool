500ff2af8d23811e4a31e7df7c4e6fd4fd05e2e0
cmbappleby <129984364+cmbappleby@users.noreply.github.com>
2025-03-29 11:29:13 -0600

    Coding workflow

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+"""
+The purpose of this tool is to extract frames from thermal drone footage that can be used to confirm user detections
+of black-tailed jackrabbits.
+
+This is the code executed by ArcGIS Pro when the geoprocessing tool is ran.
+"""
+
+
 import arcpy
 import cv2
 import os
@@ -5,41 +13,71 @@ import glob
 import re
 import pandas as pd
 from moviepy import *
+import functions
 
+
+# === USER INPUTS === #
 srt_gdb = arcpy.GetParameterAsText(0)
 vids_folder = arcpy.GetParameterAsText(1)
 overlap_frames_out_folder = arcpy.GetParameterAsText(2)
 obs_csv_fp = arcpy.GetParameterAsText(3)
 
-# Read observation CSV into pandas data frame
+# === READ CSV AND GET LIST OF SRT GDB FEATURE CLASSES === #
+obs_csv = pd.read_csv(obs_csv_fp)
 
+arcpy.env.workspace = srt_gdb
+# Only need FCs with a loop designation (ex: F3100_L3)
+srt_fc_list = arcpy.ListFeatureClasses("F*_L*")
 
-# Read SRT GDB feature classes into a list?
-
-
+# === EXTRACT FRAMES FROM DETECTION AND OVERLAPPING VIDEOS === #
 # Loop through the obs CSV (i)
+for i in range(len(obs_csv)):
+    # Prevent outputs from being added to the map
+    arcpy.env.addOutputsToMap = False
 
+    # Get filename from CSV
+    filename = obs_csv['Filename'][i]
 
-    # Use first four characters of Flight to create fp for detection fc
+    # Create fp for detection video
+    det_vid_name = filename + ".MOV"
+    det_vid_fp = os.path.join(vids_folder, det_vid_name)
 
+    # Use first four characters of Flight from obs CSV to create fp for detection fc
+    flight = filename[:4]
+    det_srt_fc_name = next((fc for fc in srt_fc_list if flight in fc), None)
+    det_srt_fc = os.path.join(srt_gdb, det_srt_fc_name)
 
     # Convert Start and End times to seconds
+    det_start_time = obs_csv['Start'][i]
+    minute, sec = map(int, det_start_time.split(":"))
+    det_start_sec = minute * 60 + sec
 
+    det_end_time = obs_csv['End'][i]
+    minute, sec = map(int, det_end_time.split(":"))
+    det_end_sec = minute * 60 + sec
+
+    # Using detection fc name, determine loop number, and create folder with loop number
+    loop_num = det_srt_fc_name.split("_")[-1]
+    frames_folder = os.path.join(overlap_frames_out_folder, loop_num)
+    os.mkdir(frames_folder)
+
+    # Extract frames from destination video using Start and End times (add "_detection{i}" to fn)
+    det_base_fn = f"{filename}_{loop_num}_detection{i}_"
+    functions.extract_frames(det_start_sec, det_end_sec, det_vid_fp, frames_folder, det_base_fn)
 
     # Select points in detection fc using Start and End time (select by start attribute)
+    det_lyr = "det_lyr"
+    arcpy.management.MakeFeature(det_srt_fc, det_lyr)
 
-
-    # Using detection fc name, determine loop number
-
-
-    # Create folder with name of loop number
-
-
-    # Extract frames from destination video using Start and End times (add "detection{i}" to fn)
-
+    arcpy.management.SelectLayerByAttribute(
+        in_layer_or_view=det_lyr,
+        selection_type="NEW_SELECTION",
+        where_clause=f"start >= {det_start_sec} And start <= {det_end_sec}",
+        invert_where_clause=None
+    )
 
     # Subset SRT fc list that have the same loop number, sans detection fc (overlap fc list)
-
+    ovlp_srt_fc_list = arcpy.ListFeatureClasses(f"*{loop_num}*")
 
     # Loop through overlap fc list
 
