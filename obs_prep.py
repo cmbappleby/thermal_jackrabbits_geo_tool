e766c03434805f9202bdab408aa9c0e5aa00f592
cmbappleby <129984364+cmbappleby@users.noreply.github.com>
2025-04-04 10:31:02 -0600

    Adjusted for ClipChamp weird times.

--- obs_prep.py
+++ obs_prep.py
@@ -1,12 +1,23 @@
 import pandas as pd
 import os
+import math
 
 
-def time_to_sec(timestamp):
+def time_to_sec(timestamp, ts_type):
     minute, sec = map(int, timestamp.split(":"))
     time_sec = minute * 60 + sec
+    if ts_type == 'End':
+        real_sec = math.ceil(time_sec * 0.6)
+    else:
+        real_sec = math.floor(time_sec * 0.6)
 
-    return time_sec
+    return real_sec
+
+
+def real_time(real_sec):
+    real_ts = f"{real_sec // 60:02}:{real_sec % 60:02}"
+    
+    return real_ts
 
 
 # === EDIT THE FOLLOWING FILEPATHS (AND SHEET NUMBERS) === #
@@ -43,8 +54,12 @@ obs_3_4_df = obs_df[obs_df['Flight_ID'] > 3000]
 obs_3_4_df = obs_3_4_df.rename(columns={'Video_Frame_Start': 'Start', 'Video_Frame_End': 'End'})
 obs_3_4_df['Start'] = pd.to_datetime(obs_3_4_df['Start'], format='%M:%S:%f').dt.strftime('%M:%S')
 obs_3_4_df['End'] = pd.to_datetime(obs_3_4_df['End'], format='%M:%S:%f').dt.strftime('%M:%S')
-obs_3_4_df['StartSec'] = obs_3_4_df['Start'].apply(time_to_sec)
-obs_3_4_df['EndSec'] = obs_3_4_df['End'].apply(time_to_sec)
+
+# Convert timestamp to seconds and adjust for ClipChamp's weird timestamps
+obs_3_4_df['StartSec'] = obs_3_4_df.apply(lambda row: time_to_sec(row['Start'], 'Start'), axis=1)
+obs_3_4_df['EndSec'] = obs_3_4_df.apply(lambda row: time_to_sec(row['End'], 'End'), axis=1)
+obs_3_4_df['Start'] = obs_3_4_df['StartSec'].apply(real_time)
+obs_3_4_df['End'] = obs_3_4_df['EndSec'].apply(real_time)
 
 # Join obs to flight and temp info
 flight_temp_obs_df = flight_temp_df.merge(obs_3_4_df, on='Flight_ID', how='right')
@@ -57,3 +72,4 @@ flight_temp_obs_df['Filename'] = flight_temp_obs_df.apply(lambda row: f"{row['Fl
 # Get folder path
 out_folder = os.path.dirname(obs_xlsx_fp)
 flight_temp_obs_df.to_csv(os.path.join(out_folder, "obs_for_geotool.csv"), index=False)
+                                                                                                                    
